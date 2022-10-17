cdbb3e3b62eb88f68ac400ba6a45b4566265e22b
Lukas <lukas.witeczek@rwth-aachen.de>
2022-10-17 21:55:27 +0200

    Pause in ch. 6 - Heroes and HTTP

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,13 +3,18 @@ import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
+
+  private heroesUrl = "api/heroes";
 
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
@@ -23,5 +28,10 @@ export class HeroService {
     const hero = HEROES.find(h => h.id === id)!;
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(hero);
+  }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 }
